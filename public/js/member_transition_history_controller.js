f16ee32042c9b7b4fd9c50883aac18c448e13bfa
1353031 <minhthien1995@gmail.com>
2016-05-13 23:08:34 +0700

    sadf

--- public/js/member_transition_history_controller.js
+++
@@ -1,8 +0,0 @@
-app.controller('transHisController',['$scope', '$http', function($scope, $http){
-	$scope.InfoUser = JSON.parse(sessionStorage.InfoUser);
-	$http.get('http://127.0.0.1:3000/list_bid/byUser/'+ $scope.InfoUser.user_id).then(function(res) {
-        if(res.data != null || res.data.length != 0){
-            $scope.listBids = res.data;
-        }
-    });
-}]);
